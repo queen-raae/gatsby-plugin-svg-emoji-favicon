ebe14cdd17978181ff31c1b4037673e491784284
Benedicte Raae <raae@raae.codes>
2021-12-06 18:39:38 +0100

    fix: key in correcy place, closes #4

--- plugin/gatsby-ssr.js
+++ plugin/gatsby-ssr.js
@@ -6,7 +6,6 @@ const React = require("react");
 const EmojiFavicon = ({ emoji }) => {
   return (
     <link
-      key="gatsby-plugin-svg-emoji-favicon"
       id="gatsby-plugin-svg-emoji-favicon"
       rel="icon"
       href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${emoji}</text></svg>`}
@@ -15,5 +14,7 @@ const EmojiFavicon = ({ emoji }) => {
 };
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  setHeadComponents([<EmojiFavicon {...pluginOptions} />]);
+  setHeadComponents([
+    <EmojiFavicon key="gatsby-plugin-svg-emoji-favicon" {...pluginOptions} />,
+  ]);
 };
